36f769b1675d2f2d4984f08725b442e4d6e13df5
jimfellows <jim.fellows01@gmail.com>
2026-02-02 05:44:06 -0800

    changing hero pic for cutter cam project

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,25 +7,9 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
-  },
-  {
     title: 'Rockfish Cutter Camera',
     description: `An overhead camera system developed for NOAA Fisheries to automate biological sampling. Features a Raspberry Pi 4, PoE, FastAPI, and custom EXIF metadata tagging for high-resolution specimen imagery.`,
-    imgSrc: '/static/images/projects/rockfish/tablet-cam.jpg',
+    imgSrc: '/static/images/projects/rockfish/fish-detect-json.png',
     href: '/blog/rockfish-camera-system',
   },
   {
